7e1c2266b215421c1beb69325e7cf48bc6bca03e
Sudershan Sridhar <42107070+Sudershan29@users.noreply.github.com>
2018-10-17 13:20:22 +0530

    Add files via upload

---
+++ Online_Rec/src/online_rec/Online_Rec.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package online_rec;
+
+/**
+ *
+ * @author sudershan
+ */
+public class Online_Rec {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       
+        Login_Page create = new Login_Page();
+        create.setVisible(true);
+        
+    }
+    
+}
